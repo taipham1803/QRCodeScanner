ba06b5362a6d524051ddc597a8d19845d771a9f2
tài phạm <taipham1803@gmail.com>
2019-07-18 09:43:37 +0700

    Initial Commit

---
+++ QR Code Scanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QR Code Scanner
+//
+//  Created by Tai Pham on 7/18/19.
+//  Copyright © 2019 Tai Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
